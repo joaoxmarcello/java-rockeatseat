422ea6d74ab8fbd7e0822b7b686298fcbe9f5045
joaoxmarcello <joaomarcello869@gmail.com>
2023-10-13 16:23:37 -0300

    correction of null properties

--- src/main/java/br/com/joaomarcello/todolist/task/TaskController.java
+++ src/main/java/br/com/joaomarcello/todolist/task/TaskController.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.var;
+import br.com.joaomarcello.todolist.utils.Utils;
 
 @RestController
 @RequestMapping("/tasks")
@@ -51,8 +51,10 @@ public class TaskController {
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUser);
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+    return this.taskRepository.save(task);
   }
 }
