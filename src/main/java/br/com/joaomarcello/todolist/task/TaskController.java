a2169baffddfca3633d71853d57d5c60f4c3f166
joaoxmarcello <joaomarcello869@gmail.com>
2023-10-14 13:22:51 -0300

    project correction

--- src/main/java/br/com/joaomarcello/todolist/task/TaskController.java
+++ src/main/java/br/com/joaomarcello/todolist/task/TaskController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.var;
 import br.com.joaomarcello.todolist.utils.Utils;
 
 @RestController
